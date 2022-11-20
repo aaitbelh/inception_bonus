990865c277ccf523b0ad43cf52a13208ebe6a3ef
ayoub aitbelhaj <aitbelhajayoub566@example.com>
2022-11-20 10:47:06 +0100

    s

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,6 +1,13 @@
 service mysql start
-# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD'";
-mysql -u root -e "create database if not exists $DB_NAME;"#-p$DB_PASSWORD
-mysql -u root -e "create user if not exists '$DB_USER'@'%' identified by '$DB_PASSWORD';" #-p$DB_PASSWORD
-mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" #-p$DB_PASSWORD
-mysql -u root -e "FLUSH PRIVILEGES;" #-p$DB_PASSWORD
+# mysql -u root -e "create database if not exists $DB_NAME;"#-p$DB_PASSWORD
+# mysql -u root -e "create user if not exists '$DB_USER'@'%' identified by '$DB_PASSWORD';" #-p$DB_PASSWORD
+# mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" #-p$DB_PASSWORD
+# mysql -u root -e "FLUSH PRIVILEGES;" #-p$DB_PASSWORD
+
+mysql -u root -e -p$DB_PASSWORD "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD'"
+mysql -u root -p$DB_PASSWORD << EOF
+create database if not exists $DB_NAME;
+create user if not exists '$DB_USER'@'%' identified by '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
